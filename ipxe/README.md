3e8e1047ed27ad352b2230e7084790d773bc2f7b
randomcoww <averyfuzzybunny@gmail.com>
2025-09-23 15:45:53 -0700

    add ipxe build

---
+++ ipxe/README.md
@@ -0,0 +1,19 @@
+### Container for iPXE images served over TFTP or HTTP
+
+https://github.com/ipxe/ipxe
+
+PXE images built to support
+
+```
+DNS
+HTTP
+HTTPS
+iSCSI
+TFTP
+FCoE
+SRP
+VLAN
+AoE
+EFI
+Menu
+```
---
+++ ipxe/config/general.h
@@ -0,0 +1,3 @@
+#define  NET_PROTO_IPV6          /* IPv6 protocol */
+#define  NET_PROTO_FCOE          /* Fibre Channel over Ethernet protocol */
+#define  DOWNLOAD_PROTO_HTTPS    /* Secure Hypertext Transfer Protocol */
